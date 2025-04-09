fb3160f3e8a9d1f3a3441e9ea33a1457f1590d05
Stephen Griffiths <github@stegriff.co.uk>
2025-04-09 13:46:22 +0100

    Improve SQL example to use checkboxes

--- src/SqlSherlock/sql/Product Setup/0. Discover ProductFamily Codes.sql
+++ src/SqlSherlock/sql/Product Setup/0. Discover ProductFamily Codes.sql
@@ -1,6 +1,9 @@
 -- Here are the product family codes matching your query.
 -- To get everything, leave the input blank
+
 declare @ProductFamilyCodeFragment nvarchar(10)
+declare @IncludeInactive bit
+
 select
 pf.ID as ProductFamilyID,
 pf.Name,
@@ -8,3 +11,4 @@ pf.Code as ProductFamilyCode,
 pf.Active
 from ProductFamily pf
 where pf.Code like '%' + @ProductFamilyCodeFragment + '%'
+and @IncludeInactive = 1 or pf.Active = 1
